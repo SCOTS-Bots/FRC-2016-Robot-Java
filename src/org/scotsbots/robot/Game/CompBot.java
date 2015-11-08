7abb36968109b9ffebe24db6236d9f796130ba96
kf72899 <keeganfockler9@gmail.com>
2015-11-08 14:29:14 -0500

    Added Base Robot

---
+++ src/org/scotsbots/robot/Game/CompBot.java
@@ -0,0 +1,28 @@
+package org.scotsbots.robot.Game;
+
+import org.scotsbots.robot.AutonStrategy;
+import org.scotsbots.robot.RobotHardware;
+
+public class CompBot extends RobotHardware {
+
+	public void initialize() {
+		
+		
+	}
+
+	public void teleop() {
+		
+		
+	}
+
+	public AutonStrategy getSwitchedAuton() {
+		
+		return null;
+	}
+
+	public String getName() {
+		
+		return null;
+	}
+
+}
