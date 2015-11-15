52584484e2f8f16656c9c5c3c87d5e3ec1b2196a
PatrickRosso <20009602@hartlandschools.us>
2015-11-15 14:43:06 -0500

    MechBot

--- src/org/scotsbots/robot/Game/MechBot.java
+++ src/org/scotsbots/robot/Game/MechBot.java
@@ -1,5 +1,34 @@
 package org.scotsbots.robot.Game;
 
-public class MechBot {
+import org.scotsbots.robot.AutonStrategy;
+import org.scotsbots.robot.RobotHardware;
+import org.scotsbots.robot.RobotOperation;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Victor;
+
+public class MechBot extends RobotHardware
+{
+	public Victor leftFront, leftBack, rightFront, rightBack;
+
+	public void initialize() {
+		leftFront = new Victor(0);
+		leftBack = new Victor(1);
+		rightFront = new Victor(2);
+		rightBack = new Victor(3);
+		drivetrain = new RobotDrive(leftFront,leftBack,rightFront,rightBack);
+	}
+
+	public void teleop() {
+		RobotOperation.driveMecanum(1);
+	}
+
+	public AutonStrategy getSwitchedAuton() {
+		return null;
+	}
+
+	public String getName() {
+		return null;
+	}
 
 }
