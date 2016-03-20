b63dd1b43c5a7cbaa7d854777cf4c77d8bcd0f92
Domenic Portuesi <dportuesi@gmail.com>
2016-03-20 17:04:54 -0400

    Compbot ready

--- src/org/scotsbots/stronghold/RobotHardwareCompbot.java
+++ src/org/scotsbots/stronghold/RobotHardwareCompbot.java
@@ -3,10 +3,12 @@ package org.scotsbots.stronghold;
 import org.scotsbots.robotbase.RobotHardware;
 import org.scotsbots.robotbase.RobotOperation;
 import org.scotsbots.robotbase.RobotVision;
+import org.scotsbots.robotbase.RobotVisionDualUSB;
 import org.scotsbots.robotbase.utils.Gamepad;
 
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Victor;
 
@@ -27,25 +29,33 @@ public class RobotHardwareCompbot extends RobotHardware
 	public Spark tapeVertical;
 	public Spark tapeHorizontal;
 	
+	//-1 to 1
+	public double speed = 1;
+	public double scoopSpeed = 0.5;
+	
+	boolean camSwitchBump = false;
+	int camSwitchBumpTime = 0;
+	
 	@Override
 	public void initialize()
 	{
-		vision = new RobotVision("10.47.76.20", "cam0");          
-		leftMotor = new Victor(0);         //changed to 0, was 1                      
-		rightMotor = new Victor(1); 	   //changed to 1, was 0                    
+		vision = new RobotVision("10.47.76.20", "cam0");
+		dualUSBVision = new RobotVisionDualUSB("cam0", "cam1");
+		
+		leftMotor = new Victor(0);                           
+		rightMotor = new Victor(1); 	                    
 		drivetrain = new RobotDrive(leftMotor, rightMotor);
 		
-		//drivetrain.setInvertedMotor(MotorType.kFrontLeft, true);
 		drivetrain.setInvertedMotor(MotorType.kRearRight, true);
 		drivetrain.setInvertedMotor(MotorType.kRearLeft, true);
 
 
-		shooterMotor = new Victor(2);   //changed to 2, WAS 3
-		scoopMotor = new Victor(4);     //changed to 4, was 2
-		scoopGate = new Victor(3);      //changed to 3, was 5
-		winch = new Victor(5);          //changed to 5, was 6
+		shooterMotor = new Victor(2);   
+		scoopMotor = new Victor(4);     
+		scoopGate = new Victor(3);      
+		winch = new Victor(5);          
 		
-		manipulator = new Spark(6);   //changed to 6, was 4
+		manipulator = new Spark(6);   
 		tapeVertical = new Spark(8);
 		tapeHorizontal = new Spark(7);
 		
@@ -54,25 +64,40 @@ public class RobotHardwareCompbot extends RobotHardware
 		addAuton(new AutonStrategyDriveReverse());
 		addAuton(new AutonStrategyDumpReverse());
 		addAuton(new AutonStrategyLowbarReverse());
+		addAuton(new AutonStrategyStraightShootBackup());
+		addAuton(new AutonStrategyStraightShootReversed());
+		
+		speed = 1;
+		scoopSpeed = 0.5;
+		SmartDashboard.putNumber("Speed", speed);
+		SmartDashboard.putNumber("Scoop Speed", scoopSpeed);
+		
+		camSwitchBump = false;
+		camSwitchBumpTime = 0;
 	}
 
 	public void teleopInit()
 	{
-		
+		speed = 1;
+		scoopSpeed = 0.5;
+		SmartDashboard.putNumber("Speed", speed);
+		SmartDashboard.putNumber("Scoop Speed", scoopSpeed);
+		camSwitchBump = false;
+		camSwitchBumpTime = 0;
 	}
 	
 	@Override
 	public void teleop()
 	{
-		RobotOperation.driveTank(1);
-
+		RobotOperation.driveTank(1, SmartDashboard.getNumber("Speed"));
+		
 		if(Gamepad.secondaryAttackJoystick.getRightT())
 		{
-			scoopMotor.set(1);
+			scoopMotor.set(-1);
 		}
 		else if(Gamepad.secondaryAttackJoystick.getRB())
 		{
-			scoopMotor.set(-1);
+			scoopMotor.set(1);
 		}
 		else
 		{
@@ -83,6 +108,10 @@ public class RobotHardwareCompbot extends RobotHardware
 		{
 			shooterMotor.set(1);
 		}
+		else if(Gamepad.secondaryAttackJoystick.getLB())
+		{
+			shooterMotor.set(-1);
+		}
 		else
 		{
 			shooterMotor.set(0);
@@ -91,6 +120,7 @@ public class RobotHardwareCompbot extends RobotHardware
 		//auto shoot
 		if(Gamepad.secondaryAttackJoystick.getY())
 		{
+			//if it is in the right location
 			autoFireMode = true;
 		}
 		
@@ -102,36 +132,39 @@ public class RobotHardwareCompbot extends RobotHardware
 			}
 		}
 		
-		if(Gamepad.secondaryAttackJoystick.getB())
-		{
-			manipulator.set(-1);
-		}
-		else if(Gamepad.secondaryAttackJoystick.getX())
-		{
-			manipulator.set(1);
-		}
-		else
-		{
-			manipulator.set(0);
-		}
-		
 		if(Gamepad.secondaryAttackJoystick.getA())
 		{
-			winch.set(1);
+			winch.set(-1);
 		}
 		else if(Gamepad.secondaryAttackJoystick.getStart())
 		{
-			winch.set(-1);
+			winch.set(1);
 		}
 		else
 		{
 			winch.set(0);
 		}
-				
-		scoopGate.set(Gamepad.secondaryAttackJoystick.getLeftY() * 0.5);
-				
+								
 		tapeVertical.set(Gamepad.secondaryAttackJoystick.getRightY() * -1);
 		tapeHorizontal.set(Gamepad.secondaryAttackJoystick.getRightX() * 0.5);
+		
+		if(camSwitchBump && Gamepad.secondaryAttackJoystick.getSelect())
+		{	
+			dualUSBVision.switchCameras();
+			camSwitchBump = false;
+		}
+		
+		if(!camSwitchBump)
+		{
+			camSwitchBumpTime++;
+			if(camSwitchBumpTime >100)
+			{
+				camSwitchBump = true;
+				camSwitchBumpTime = 0;
+			}
+		}
+		
+		scoopMotor.set(Gamepad.secondaryAttackJoystick.getLeftY() * SmartDashboard.getNumber("Scoop Speed"));
 	}
 
 	/**
@@ -141,11 +174,11 @@ public class RobotHardwareCompbot extends RobotHardware
 	public boolean autoShoot()
 	{
 		autoShootTimer++;
-		if(autoShootTimer < 7)
+		if(autoShootTimer < 5)
 		{
 			scoopMotor.set(1);
 		}
-		else if(autoShootTimer > 75 && autoShootTimer < 175)
+		else if(autoShootTimer > 50 && autoShootTimer < 150)
 		{
 			scoopMotor.set(-1);
 		}
@@ -171,7 +204,13 @@ public class RobotHardwareCompbot extends RobotHardware
 		return false;
 	}
 	
-	public boolean usesSingleUSBCamera()
+	public void logSmartDashboard()
+	{
+		SmartDashboard.putNumber("Speed", speed);
+		super.logSmartDashboard();
+	}
+	
+	public boolean usesDualUSBCameras()
 	{
 		return true;
 	}
